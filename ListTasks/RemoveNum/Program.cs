40c8d8ba5ddb9c0d093a0d5303f91cd88f889692
KChusheva <k.chusheva2019@pmg-pernik.com>
2023-02-14 13:07:58 +0200

    add task1

---
+++ ListTasks/RemoveNum/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RemoveNum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> nums = Console.ReadLine().Split().Select(int.Parse).ToList();
+            var last = nums[nums.Count - 1];
+            nums.Remove(last);
+            nums.RemoveAll(x => x == last);
+            Console.WriteLine(string.Join(" ", nums));
+
+        }
+    }
+}
